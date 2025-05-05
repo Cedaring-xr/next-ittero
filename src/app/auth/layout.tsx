70be37ce12de0fd24683bc22efab097a9b882f73
mattraydev <cedaring.xr@gmail.com>
2025-05-05 07:22:03 -0600

    page and auth updates

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,12 +3,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<main className="flex items-center justify-center md:h-screen">
-			<div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5">
-				<div>
-					<div>logo</div>
-				</div>
-				{children}
-			</div>
+			<div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5">{children}</div>
 		</main>
 	)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
 			<div className="mt-4 flex grow flex-col gap-4 md:flex-row">
 				<div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
 					<p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-						<strong>Welcome to Acme.</strong>
+						<strong>Welcome to Ittero.</strong>
 					</p>
 					<Link
 						href="/auth/login"
@@ -24,7 +24,7 @@ export default function Page() {
 				</div>
 				<div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
 					<Image
-						src="/hero-desktop.png"
+						src="/journal.png"
 						width={1000}
 						height={760}
 						className="hidden md:block"
