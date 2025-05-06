dc987ba08657c063fe927db5611ac9a1f3edfa99
mattraydev <cedaring.xr@gmail.com>
2025-05-06 08:11:10 -0600

    fix build errors

--- src/ui/dashboard/nav-links.tsx
+++ src/ui/dashboard/nav-links.tsx
@@ -1,21 +1,43 @@
+'use client'
+import { UserGroupIcon, HomeIcon, DocumentDuplicateIcon } from '@heroicons/react/24/outline'
+import clsx from 'clsx'
 import Link from 'next/link'
-import NavLinks from '@/ui/dashboard/nav-links'
-import AcmeLogo from '@/ui/acme-logo'
-import LogoutForm from '@/ui/dashboard/logout-form'
+import { usePathname } from 'next/navigation'
 
-export default function SideNav() {
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+	{ name: 'Home', href: '/dashboard', icon: HomeIcon },
+	{
+		name: 'Invoices',
+		href: '/dashboard/invoices',
+		icon: DocumentDuplicateIcon
+	},
+	{ name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon }
+]
+
+export default function NavLinks() {
+	const pathname = usePathname()
 	return (
-		<div className="flex h-full flex-col px-3 py-4 md:px-2">
-			<Link className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40" href="/">
-				<div className="w-32 text-white md:w-40">
-					<AcmeLogo />
-				</div>
-			</Link>
-			<div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-				<NavLinks />
-				<div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-				<LogoutForm />
-			</div>
-		</div>
+		<>
+			{links.map((link) => {
+				const LinkIcon = link.icon
+				return (
+					<Link
+						key={link.name}
+						href={link.href}
+						className={clsx(
+							'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+							{
+								'bg-sky-100 text-blue-600': pathname === link.href
+							}
+						)}
+					>
+						<LinkIcon className="w-6" />
+						<p className="hidden md:block">{link.name}</p>
+					</Link>
+				)
+			})}
+		</>
 	)
 }
