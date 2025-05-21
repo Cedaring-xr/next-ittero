5fcdd341a70e8dd1d072aa8a7a8c2455f9a04811
cedaring-xr <cedaring.xr@gmail.com>
2025-05-21 15:58:19 -0600

    added playwright setup and basic tests. failing on webkit

---
+++ tests/example.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test'
+ 
+test('should navigate to the sign in page', async ({ page }) => {
+  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+  await page.goto('http://localhost:3000/')
+  // Find an element with the text 'About' and click on it
+  await page.click('text=Log In')
+  // The new URL should be "/auth/login" (baseURL is used there)
+  await expect(page).toHaveURL('http://localhost:3000/auth/login')
+  // The new page should contain an h1 with "Please log in to continue."
+  await expect(page.locator('h1')).toContainText('Please log in to continue.')
+})
