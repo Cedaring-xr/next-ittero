7c9adba10bc6c1c21f28ca184c447b484acfdb5c
mattraydev <cedaring.xr@gmail.com>
2025-08-22 22:18:44 -0600

    minor page route changes

---
+++ src/app/dashboard/journal/newJournal/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function page() {
+	return <div>New Journal entry page</div>
+}
+
+export default page
--- src/app/journal/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function page() {
-  return (
-    <div>Quick Journal</div>
-  )
-}
-
-export default page
