08a311b50526fffaf830cb33b16e2312f550ae44
mattraydev <cedaring.xr@gmail.com>
2025-10-12 21:37:30 -0600

    updates to new journal page, next version update

--- src/app/dashboard/journal/newJournal/page.tsx
+++ src/app/dashboard/journal/newJournal/page.tsx
@@ -1,12 +1,24 @@
 'use client'
 import dynamic from 'next/dynamic'
 import { lusitana } from '@/ui/fonts'
-import React from 'react'
-// import Banner from '@/ui/info/banner'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { MdArrowRightAlt } from 'react-icons/md'
 
 const Banner = dynamic(() => import('@/ui/info/banner'), { ssr: false })
 
-function page() {
+export type FormData = {
+	name: string
+	email: string
+	message: string
+}
+
+export default function NewJournal() {
+	const [sendForm, setSendForm] = useState(false)
+	const { register, handleSubmit } = useForm<FormData>()
+
+	function onSubmit(data: FormData) {}
+
 	return (
 		<>
 			<div id="journal container">
@@ -41,6 +53,69 @@ function page() {
 							date dropdown
 						</select>
 					</form>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<h2 className="flex justify-center text-xl serif-font text-cyan-400">office@radiantpine.com</h2>
+						{sendForm ? (
+							<div className="h-[350px] mt-24">
+								<h3 className="mx-4 serif-font text-xl">
+									<span className="font-bold">Thank you for your interest! </span>
+									<br />
+									Your message has been recieved and I will respond by email as soon as I am able.
+								</h3>
+							</div>
+						) : (
+							<div>
+								<div className="my-2">
+									<label htmlFor="name" className="mb-2 block text-base font-medium">
+										Name
+									</label>
+									<input
+										type="text"
+										placeholder="Full Name"
+										className="w-full rounded-md border border-gray-300 bg-[#f7f4fb] pt-2 px-4 text-base font-medium text-gray-700 outline-none focus:border-2 focus:border-[#c524a8] focus:shadow-md"
+										{...register('name', { required: true })}
+									/>
+								</div>
+								<div className="mb-5">
+									<label htmlFor="email" className="mb-2 block text-base font-medium text-white">
+										Email Address
+									</label>
+									<input
+										type="email"
+										placeholder="example@domain.com"
+										className="w-full rounded-md border border-gray-300 bg-[#f7f4fb] pt-2 px-4 text-base font-medium text-gray-700 outline-none focus:border-2 focus:border-[#c524a8] focus:shadow-md"
+										{...register('email', { required: true })}
+									/>
+								</div>
+								<div className="mb-5">
+									<label htmlFor="message" className="mb-2 block text-base font-medium text-white">
+										Message
+									</label>
+									<textarea
+										rows={4}
+										placeholder="Type your message"
+										className="w-full resize-none rounded-md border border-gray-300 bg-white pt-2 px-6 text-base font-medium text-gray-700 outline-none focus:border-2 focus:border-[#c524a8] focus:shadow-md"
+										{...register('message', { required: true })}
+									></textarea>
+								</div>
+								<div>
+									<button
+										className="hover:shadow-form text-xl  headline-font text-black"
+										type="submit"
+									>
+										<div id="button-wrapper" className="button-wrap-shadow">
+											<div className="button-gradient button-clip button-shadow p-1">
+												<div className="button-clip bg-[#f7f4fb] px-4 py-0 items-center flex hover:bg-[#121313] hover:text-[#89f7fe]">
+													Submit
+													<MdArrowRightAlt className="text-4xl ml-2"> </MdArrowRightAlt>
+												</div>
+											</div>
+										</div>
+									</button>
+								</div>
+							</div>
+						)}
+					</form>
 				</div>
 				<div>
 					<button>View past entries</button>
@@ -50,5 +125,3 @@ function page() {
 		</>
 	)
 }
-
-export default page
--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import Link from 'next/link'
+import { lusitana } from '@/ui/fonts'
+import { HiOutlineClipboardList } from 'react-icons/hi'
 
-function page() {
-	return <div>Frequently Asked Questions</div>
+export default function FAQpage() {
+	return (
+		<>
+			<>
+				<div className={`${lusitana.className} font-bold flex flex-col p-6 gap-6`}>
+					<Link href="/">
+						<div className="bg-gray-50 text-gray-900 rounded-lg h-[100px] hover:border-2 hover:border-indigo-700 pt-6 pl-4 flex hover:text-indigo-700">
+							<h3 className="md:text-xl mt-2">Home</h3>
+						</div>
+					</Link>
+					<div className="bg-gray-50 text-gray-900 rounded-lg  hover:border-2 hover:border-indigo-700 pt-6 pl-4 flex flex-col hover:text-indigo-700">
+						<h3 className="text-xl bold mb-4">Frequently Asked Questions</h3>
+						<div className="flex flex-col">
+							<ul>
+								<li>What does it take to create an account</li>
+								<li>What happens to my information</li>
+								<li>How will I be contacted for notifications</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</>
+		</>
+	)
 }
-
-export default page
